1899dd9914aecbfa7f6e61c7944cc27d4292f4a6
Michael Drejer Andersen <mich450m@student.eadania.dk>
2019-04-11 09:44:22 +0200

    stepper commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule , } from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatStepperModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { StepperComponent } from './stepper/stepper.component';
+
 
 
 
@@ -10,11 +14,14 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    StepperComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    MatStepperModule, MatFormFieldModule, MatInputModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
+    BrowserAnimationsModule
 
   ],
   providers: [],
