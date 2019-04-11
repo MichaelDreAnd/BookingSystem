2b23a154c329deba5f0bf5379b7f90dee4f4db3c
Michael Drejer Andersen <mich450m@student.eadania.dk>
2019-04-11 11:26:44 +0200

    date time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule , } from '@angular/platform-browser';
-import {ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatStepperModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatStepperModule, MatFormFieldModule, MatInputModule, MatNativeDateModule } from '@angular/material';
 import { StepperComponent } from './stepper/stepper.component';
+import { DateComponent } from './booking/date/date.component';
+import { MatDatepickerModule, } from '@angular/material/datepicker';
+import { TimeComponent } from './booking/time/time.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -14,14 +18,19 @@ import { StepperComponent } from './stepper/stepper.component';
 @NgModule({
   declarations: [
     AppComponent,
-    StepperComponent
+    StepperComponent,
+    DateComponent,
+    TimeComponent
   ],
   imports: [
     ReactiveFormsModule,
     MatStepperModule, MatFormFieldModule, MatInputModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    NgbModule
 
   ],
   providers: [],
